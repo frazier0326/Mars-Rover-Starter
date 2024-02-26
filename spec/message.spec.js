b2ec346c31026c37f049322e8861c0b63be44602
frazier0326 <frazier0326@gmail.com>
2024-02-26 03:30:03 -0600

    wip

--- spec/message.spec.js
+++ spec/message.spec.js
@@ -5,5 +5,18 @@ const Command = require('../command.js');
 //       However, do NOT edit the grading tests for any reason and make sure to un-comment out your code to get the autograder to pass.
 
 describe("Message class", function() {
-
+    it("throws error if a name is NOT passed into the constructor as the first parameter", function() {
+        expect( function() { new Message();}).toThrow(new Error('Message required.'));
+    });
+    it("constructor sets name", function() {
+        let set = new Message('Message');
+        expect(set.name).toEqual('Message');
+    });
+    it("contains a commands array passed into the constructor as the 2nd argument", function() {
+        let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+        let message = new Message('Test message with two commands', commands);
+        expect(message.commands).toEqual([new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')])
+        
+        
+    })
 });
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -7,7 +7,35 @@ const Command = require('../command.js');
 
 
 describe("Rover class", function() {
-
   // 7 tests here!
+  it("constructor sets position and default values for mode and generatorWatts", function () {
+    let rover = new Rover(1);
+    expect(rover.position).toBe(1);
+    expect(rover.mode).toBe('NORMAL');
+    expect(rover.generatorWatts).toBe(110);
+  });
+  //8 “response returned by receiveMessage contains the name of the message”
+  it("response returned by receiveMessage contains the name of the message", function() {
 
+  })
+  //9
+  it("response returned by receiveMessage includes two results if two commands are sent in the message", function() {
+
+  });
+  //10
+  it("responds correctly to the status check command", function() {
+
+  });
+  //11
+  it("responds correctly to the mode change command", function() {
+
+  });
+  //12
+  it("responds with a false completed value when attempting to move in LOW_POWER mode", function() {
+
+  });
+  //13
+  it("responds with the position for the move command", function() {
+
+  });
 });
