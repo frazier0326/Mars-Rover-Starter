f5ee0252095d1afaef64a8b39a0b65114651b479
frazier0326 <frazier0326@gmail.com>
2024-02-29 17:34:19 -0600

    updated rover class and updated test

--- rover.js
+++ rover.js
@@ -11,12 +11,28 @@ class Rover {
    receiveMessage(message) {
       let response = {
       message: message.name,
-      results:[
-
-         
-      ]
+      results:[]
       };
-      
+      for (let i=0; i<message.commands.length; i++) {
+         if(message.commands[i].commandType === 'STATUS_CHECK') {
+         response.results.push({completed: true, 
+         roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, 
+            position: this.position}});
+      } else if (message.commands[i].commandType === 'MODE_CHANGE'){
+         this.mode = message.commands[i].value;
+         response.results.push({completed: true});
+      } else if (message.commands[i].commandType === 'MOVE') {
+         if(this.mode === 'LOW_POWER') {
+            response.results.push({completed: false});
+         } else {
+            this.position = message.commands[i].value;
+            response.results.push({completed: true});
+         }
+      } else {
+         response.results.push({completed: true});
+      }
+   }
+
       return response;
    }
 
